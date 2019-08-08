cc9f7cefd22337304629560c0b4f85aadcdd6d45
Denis Isidoro <denis_isidoro@live.com>
2019-08-08 16:03:49 -0300

    /root

--- klapaucius/background.sh
+++ klapaucius/background.sh
@@ -1,6 +1,6 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
 
-export DOTFILES="${HOME}/.dotfiles"
+export DOTFILES="/root/.dotfiles"
 export PATH="${DOTFILES}/bin/dot:${PATH}"
 
 git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
