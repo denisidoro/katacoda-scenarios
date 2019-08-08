231337a401d960c8b9a604e26802f8d37c8ee7e7
Denis Isidoro <denis_isidoro@live.com>
2019-08-08 16:35:53 -0300

    secrets

--- klapaucius/foreground.sh
+++ klapaucius/foreground.sh
@@ -6,3 +6,18 @@ git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
 "${DOTFILES}/bin/dot" pkg add lein
 
 exec $SHELL
+
+echo "Please insert the password:"
+read pass
+
+mkdir -p ~/.ssh
+
+cat ~/.secrets/token_private.txt \
+  | dot sec decrypt -p "$pass" \
+  > ~/.ssh/id_rsa
+
+cat ~/.secrets/token_public.txt \
+  | dot sec decrypt -p "$pass" \
+  > ~/.ssh/id_rsa.pub
+
+git clone git@github.com:denisidoro/klapaucius.git
