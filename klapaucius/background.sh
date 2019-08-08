ab98169ec536cff90c2610e114e06b39dcecbc84
Denis Isidoro <denis_isidoro@live.com>
2019-08-08 16:07:35 -0300

    debug 2

--- klapaucius/background.sh
+++ klapaucius/background.sh
@@ -1,1 +1,9 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
+
+export DOTFILES="/root/.dotfiles"
+export PATH="${DOTFILES}/bin/dot:${PATH}"
+
+echo "${DOTFILES};${PATH};$(whoami);${HOME}" > /root/log.txt
+
+git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
+"${DOTFILES}/bin/dot" pkg add lein
--- klapaucius/foreground.sh
+++
@@ -1,5 +0,0 @@
-export DOTFILES="/root/.dotfiles"
-export PATH="${DOTFILES}/bin/dot:${PATH}"
-
-git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
-"${DOTFILES}/bin/dot" pkg add lein
