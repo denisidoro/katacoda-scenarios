12b0f67133001fc9ce06396badba0555637e22a7
Denis Isidoro <denis_isidoro@live.com>
2019-08-08 16:26:11 -0300

    no background

--- klapaucius/background.sh
+++ klapaucius/background.sh
@@ -1,10 +1,1 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-
-export DOTFILES="${HOME}/.dotfiles"
-export DOT_DOCOPTS=go
-export PATH="${DOTFILES}/bin/dot:${PATH}"
-
-git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
-"${DOTFILES}/bin/dot" pkg add lein
-
-exec $SHELL
