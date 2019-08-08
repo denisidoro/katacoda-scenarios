8d540777afccba3b158be945818c75904db3d69d
Denis Isidoro <denis_isidoro@live.com>
2019-08-08 16:13:35 -0300

    assets

--- klapaucius/background.sh
+++ klapaucius/background.sh
@@ -1,9 +1,10 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
 
-export DOTFILES="/root/.dotfiles"
+export DOTFILES="${HOME}/.dotfiles"
 export PATH="${DOTFILES}/bin/dot:${PATH}"
-
-echo "${DOTFILES};${PATH};$(whoami);${HOME}" > /root/log.txt
+export DOT_DOCOPTS=go
 
 git clone https://github.com/denisidoro/dotfiles "$DOTFILES"
 "${DOTFILES}/bin/dot" pkg add lein
+
+exec $SHELL
